c8d7f8b85bb8a04230103280a78816ee2dcb3b1a
crisgustavo <cris.gustavogs@gmail.com>
2024-10-17 08:40:33 -0300

    Romoção de código comentado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,9 +11,6 @@ import Whatsapp from '../../components/Whatsapp'
 
 import { ImgSlider, Content, BudgetInfo,
     BudgetText, BudgetButton, BudgetTitle,
-    /*ContentInfo, ContentTitle, ContentText,
-    ContentShow, ContentBox,
-    ContentCarroussel, ContentH1*/
 } from "./styles"
 
 
@@ -22,14 +19,7 @@ import slider2 from '../../assets/midias/banner/Ban-certificado.jpg'
 import slider3 from '../../assets/midias/banner/Ban-restaurantes.jpeg'
 import slider4 from '../../assets/midias/banner/Ban-autocenter.png'
 import slider5 from '../../assets/midias/banner/Ban-empresarial.png'
-/*import product1 from '../../assets/midias/produto/b-empresarial.jpg'
-import product2 from '../../assets/midias/produto/b-mercados.jpg'
-import product3 from '../../assets/midias/produto/b-restaurantes.jpg'
-import product4 from '../../assets/midias/produto/b-autocenter.jpg'
-import product5 from '../../assets/midias/produto/b-agrovet.jpg'
-import product6 from '../../assets/midias/produto/b-joias.jpg'
-import product7 from '../../assets/midias/produto/b-madeira.jpg'
-import product8 from '../../assets/midias/produto/b-imobiliaria.jpg'*/
+
 
 
 
@@ -79,58 +69,7 @@ function Home() {
                 </BudgetInfo>
             </Content>
 
-            {/*<Content>
-                <ContentInfo>
-                    <ContentBox>
-                        <ContentTitle>Saiba mais sobre os <a href=''>Softwares</a> Calory</ContentTitle>
-                        <ContentText>Um de nossos softwares pode ser a solução para sua empresa</ContentText>
-
-                        <ContentShow>
-                            <ContentCarroussel>
-                                <div className="item" style={{ '--delay': '1s' }} onClick={() => navigate('/softwares/lojas')}>
-                                    <img src={product1} alt="Empresarial Lojas" />
-                                    <ContentH1>Empresarial Lojas</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '2s' }} onClick={() => navigate('/softwares/mercado')}>
-                                    <img src={product2} alt="Empresarial Mercados e Conveniências" />
-                                    <ContentH1>Empresarial Mercados e Conveniências</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '3s' }} onClick={() => navigate('/softwares/restaurante')}>
-                                    <img src={product3} alt="Empresarial Restaurantes e Lanchonetes" />
-                                    <ContentH1>Empresarial Restaurantes e Lanchonetes</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '4s' }} onClick={() => navigate('/softwares/autocenter')}>
-                                    <img src={product4} alt="Empresarial Autocenter" />
-                                    <ContentH1>Empresarial Autocenter</ContentH1>
-                                </div>
-
-                            </ContentCarroussel>
-
-                            <ContentCarroussel>
-                                <div className="item" style={{ '--delay': '1s' }} onClick={() => navigate('/softwares/agrovet')}>
-                                    <img src={product5} alt="Empresarial Agro e Vet" />
-                                    <ContentH1>Empresarial Agro e Veterinária</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '2s' }} onClick={() => navigate('/softwares/joalheria')}>
-                                    <img src={product6} alt="Empresarial Joalheria" />
-                                    <ContentH1>Empresarial Joalheria</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '3s' }} onClick={() => navigate('/softwares/madeireira')}>
-                                    <img src={product7} alt="Empresarial Madeireira" />
-                                    <ContentH1>Empresarial Madeireira</ContentH1>
-                                </div>
-                                <div className="item" style={{ '--delay': '4s' }} onClick={() => navigate('/softwares/imobiliaria')}>
-                                    <img src={product8} alt="Empresarial Imobiliaria" />
-                                    <ContentH1>Empresarial Imobiliaria</ContentH1>
-                                </div>
-                            </ContentCarroussel>
-                        </ContentShow>
-                    </ContentBox>
-
-                </ContentInfo>
-
-            </Content>
-            */}
+            
             <Footer />
 
             <Whatsapp />
