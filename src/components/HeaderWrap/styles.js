790886047b834b650bdcd7c3d8aea15042aebf0c
crisgustavo <cris.gustavogs@gmail.com>
2024-10-18 10:13:18 -0300

    Atualização de layout

--- src/components/HeaderWrap/styles.js
+++ src/components/HeaderWrap/styles.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
 export const DefaultHeaderWrap = styled.div `
+    display: flex;
     width: 100%;
+    margin-top: 1%;
+
+    @media(max-width: 800px){
+        margin-top: 0;
+    }
 `
